420f139f2c4a8021116cb0cf663ca1b88aecfb61
JSE955 <jordanscottedwards@gmail.com>
2022-08-03 23:15:57 -0400

    Complete getComputerChoice()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let value = Math.floor(Math.random() * 3);
+    if (value === 0) {
+        return 'Rock';
+    } else if (value === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
