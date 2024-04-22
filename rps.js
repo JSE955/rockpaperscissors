2304a91d52cdd602b3a6b31168600d695e7096af
JSE955 <jordanscottedwards@gmail.com>
2024-04-22 10:56:11 -0400

    Add function to get human choice

--- rps.js
+++ rps.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let choice = parseInt(prompt("Choose 1 for Rock, 2 for Paper, 3 for Scissors"));
+    switch (choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            return "Invalid choice";
+            break;
+    }
+}
