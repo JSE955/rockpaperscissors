29169588ab052cca4e081bf8fecad5d89c950ffa
JSE955 <jordanscottedwards@gmail.com>
2024-04-22 11:12:04 -0400

    Add function to play round of game

--- rps.js
+++ rps.js
@@ -27,5 +27,39 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock!");
+            computerScore++;
+        } else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper!");
+            computerScore++;
+        } else if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper!");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
